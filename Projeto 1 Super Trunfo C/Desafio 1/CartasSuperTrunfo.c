73cfbfeb37ed6422a6e2751d03ff5681f89d51c5
Diógenes Ivo <diogenesrochaivo@gmail.com>
2025-01-22 19:31:31 -0300

    Ajuste de Comentarios

--- Projeto 1 Super Trunfo C/Desafio 1/CartasSuperTrunfo.c
+++ Projeto 1 Super Trunfo C/Desafio 1/CartasSuperTrunfo.c
@@ -4,8 +4,6 @@
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
 
  int main() {
     /*Declaração de Variaveis*/
@@ -18,8 +16,6 @@
 
     int i;
     // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     /*Criando Funções para Menu*/
     while (menu != 4)
     {
